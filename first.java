b59ceccf980e701c66b4256e6ff2bed811134c78
Alina <116493076+a29lina@users.noreply.github.com>
2023-04-04 20:53:55 +0300

    Update first.java

--- first.java
+++ first.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class first {
-           // Метод, который добавляет номера в книгу
            public static void addNumber(String key, int value, Map<String, ArrayList<Integer>> map){
             if (map.containsKey(key)) {
                 map.get(key).add(value);
@@ -16,7 +15,6 @@ public class first {
             }
     
         }
-        // Метод, который печатает список контактов
         public static void printBook(Map<String, ArrayList<Integer>> map){
             for (var item : map.entrySet()) {
                 String phones = "";
